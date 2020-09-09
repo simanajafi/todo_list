d921fba80d1f9c2709ec00b3f57660477aca0b98
Sima <simanajafi@gmail.com>
2020-09-09 15:14:42 +0430

    axios interceptor

--- src/Api/todos.js
+++ src/Api/todos.js
@@ -5,4 +5,21 @@ const instance = axios.create({
     timeout : 5000
 })
 
+instance.interceptors.request.use(function (config) {
+    console.log(config)
+    return config
+}, function (err) {
+    console.log(err)
+    return Promise.reject(err)
+})
+
+instance.interceptors.response.use(function (response) {
+    console.log(response)
+    return response
+}, function (err) {
+    console.log(err)
+    return Promise.reject(err)
+})
+
+
 export default instance;
