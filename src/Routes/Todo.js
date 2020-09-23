3e8663266bc8cfdd27b08ee4dfe0bf52d4458e6f
Sima <simanajafi@gmail.com>
2020-09-23 21:36:33 +0330

    single todo detail

--- src/Routes/Todo.js
+++ src/Routes/Todo.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import TodoApi from './../Api/todos'
 
-class Todo extends React.Component {
-    render() {
-        console.log(this.props.match.params)
-        return (
-            <h2>Todo Detail</h2>
-        )
-    }
+function Todo(props) {
+    const params = useParams()
+    const [todo, setTodo] = useState({})
+    const [loading, setLoading] = useState()
+
+    useEffect(() => {
+        setLoading(true)
+        console.log(params)
+        TodoApi.get(`/todos/${params.todo}.json`)
+            .then(response => {
+                setLoading(false)
+                console.log(response)
+                setTodo({ ...response.data, key: params.todo })
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    {
+                        loading
+                            ? <h2>Loading ...</h2>
+                            : (
+                                <>
+                                    <h2>Todo Detail</h2>
+                                    <p>{todo.text}</p>
+                                    <span className={`badge ${todo.done ? 'badge-success' : 'badge-warning'}`}>
+                                        {todo.done ? 'done' : 'undone'}
+                                    </span>
+                                </>
+                            )
+                    }
+                </div>
+            </div>
+        </div>
+    )
 }
-
 export default Todo;
